2f637e415ae3b9f2a90e5a84c828451132de056f
deloa-hmdro <2728068572@qq.com>
2024-06-03 14:01:01 +0800

    Delete cfltax_uu.lua

--- cfltax_uu.lua
+++
@@ -1,9 +0,0 @@
-while task.wait() do
-function identifyexecutor()
-return "Synapse X"
-end
-function getexecutorname()
-return "Synapse X"
-end
-end
-game:GetService("StarterGui"):SetCore("SendNotification", { 	Title = "Synapse X";	Text = "制作者:龙叔";	Icon = "rbxassetid://14369638300"})
